b4467f7d69939aa7e27121655383dffe3b80e501
fraguival <fran@fraguival.com>
2023-04-19 09:56:17 -0700

    agregar

---
+++ sass/js/main.js
@@ -0,0 +1,8 @@
+let btnMenu = document.getElementById('btnMenu')
+let menu = document.getElementById('menu')
+
+btnMenu.addEventListener('click', () => {
+        'use strict'
+
+        menu.classList.toggle('mostrar')
+})
